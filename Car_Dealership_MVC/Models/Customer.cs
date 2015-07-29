1e4c21693ac9faf4b21501684d98f178cdd8261e
shamseen <shamseen.c@gmail.com>
2015-07-29 09:41:05 -0400

    customer model now has a password and carID

--- Car_Dealership_MVC/Models/Customer.cs
+++ Car_Dealership_MVC/Models/Customer.cs
@@ -13,9 +13,11 @@ namespace Car_Dealership_MVC.Models
         [Key] //this will make UserName the unique identifier for each customer
         public string UserName { get; set; }
 
-        string Name { get; set; }    
+        public string password { get; set; }
 
-        string carVin  { get; set; }
+        public string Name { get; set; }    
+
+        public int carID  { get; set; }
     }
 
 
